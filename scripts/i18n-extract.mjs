b73e832dc42986573a065428343f14e8fd83726e
果农 <htmambo@gmail.com>
2026-03-22 17:07:30 +0800

    fix(i18n): fail extraction cleanly without msgcat

--- scripts/i18n-extract.mjs
+++ scripts/i18n-extract.mjs
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import sh from 'shelljs'
 import fs from 'node:fs/promises'
+import { execFileSync } from 'node:child_process'
 import * as vars from './vars.mjs'
 import log from 'npmlog'
 import { GettextExtractor, JsExtractors, HtmlExtractors } from 'gettext-extractor'
@@ -13,41 +14,51 @@ const tempHtml = 'locale/tmp-html'
 
 ;(async () => {
     sh.mkdir('-p', tempHtml)
-    for (const plugin of vars.builtinPlugins) {
-        log.info('compile-pug', plugin)
+    try {
+        for (const plugin of vars.builtinPlugins) {
+            log.info('compile-pug', plugin)
 
-        sh.exec(`yarn pug --doctype html -s --pretty -O '{require: function(){}}' -o ${tempHtml}/${plugin} ${plugin}`, { fatal: true })
+            sh.exec(`yarn pug --doctype html -s --pretty -O '{require: function(){}}' -o ${tempHtml}/${plugin} ${plugin}`, { fatal: true })
+        }
+
+        log.info('extract-ts')
+        extractor.createJsParser([
+            JsExtractors.callExpression('this.translate.instant', {
+                arguments: { text: 0 },
+            }),
+            JsExtractors.callExpression('translate.instant', {
+                arguments: { text: 0 },
+            }),
+            JsExtractors.callExpression('_', {
+                arguments: { text: 0 },
+            }),
+        ]).parseFilesGlob('./tabby-*/src/**/*.ts')
+
+        log.info('extract-pug')
+        const options = {
+            attributes: {
+                context: 'translatecontext',
+            },
+        }
+        extractor.createHtmlParser([
+            HtmlExtractors.elementContent('translate, [translate=""]', options),
+            HtmlExtractors.elementAttribute('[translate*=" "]', 'translate', options),
+        ]).parseFilesGlob(`${tempHtml}/**/*.html`)
+
+        extractor.savePotFile(tempOutput)
+        extractor.printStats()
+
+        const normalizedPot = execFileSync('msgcat', ['-s', tempOutput], {
+            cwd: process.cwd(),
+            encoding: 'utf-8',
+            stdio: ['ignore', 'pipe', 'inherit'],
+        })
+        await fs.writeFile(pot, normalizedPot)
+    } finally {
+        sh.rm('-r', tempHtml)
+        await fs.rm(tempOutput, { force: true })
     }
-
-    log.info('extract-ts')
-    extractor.createJsParser([
-        JsExtractors.callExpression('this.translate.instant', {
-            arguments: { text: 0 },
-        }),
-        JsExtractors.callExpression('translate.instant', {
-            arguments: { text: 0 },
-        }),
-        JsExtractors.callExpression('_', {
-            arguments: { text: 0 },
-        }),
-    ]).parseFilesGlob('./tabby-*/src/**/*.ts')
-
-    log.info('extract-pug')
-    const options = {
-        attributes: {
-            context: 'translatecontext',
-        },
-    }
-    extractor.createHtmlParser([
-        HtmlExtractors.elementContent('translate, [translate=""]', options),
-        HtmlExtractors.elementAttribute('[translate*=" "]', 'translate', options),
-    ]).parseFilesGlob(`${tempHtml}/**/*.html`)
-
-    extractor.savePotFile(tempOutput)
-    extractor.printStats()
-
-    sh.rm('-r', tempHtml)
-    sh.exec(`msgcat -s ${tempOutput} > ${pot}`, { fatal: true })
-
-    await fs.rename(tempOutput, pot)
-})()
+})().catch(error => {
+    log.error('i18n', error instanceof Error ? error.message : String(error))
+    process.exitCode = 1
+})
