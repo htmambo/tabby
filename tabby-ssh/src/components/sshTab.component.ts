19cabe990b2defb7bc9c368db9c6ee76c5ef060a
Utkarsh Adhran <utkarsh.adhran@openfutures.co.in>
2026-05-08 02:03:23 +0530

    feat(ssh): add hotkey to open SFTP panel (#11106)
    Co-authored-by: Eugene <inbox@null.page>

--- tabby-ssh/src/components/sshTab.component.ts
+++ tabby-ssh/src/components/sshTab.component.ts
@@ -101,6 +101,9 @@ export class SSHTabComponent extends ConnectableTerminalTabComponent<SSHProfile>
                         this.ssh.launchWinSCP(this.sshSession)
                     }
                     break
+                case 'open-sftp':
+                    this.openSFTP()
+                    break
             }
         })
 
