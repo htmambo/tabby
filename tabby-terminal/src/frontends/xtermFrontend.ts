555c1512f7f14aa74fd73abbe762dedc48d52f0e
Eugene <inbox@null.page>
2026-06-16 12:26:11 +0200

    Reapply "fix: restore terminal redraw after tab reactivation (#11275)"
    This reverts commit b5fa8111376d05f281a517faed23c4ef54302c8c.

--- tabby-terminal/src/frontends/xtermFrontend.ts
+++ tabby-terminal/src/frontends/xtermFrontend.ts
@@ -160,7 +160,7 @@ export class XTermFrontend extends Frontend {
         this.notifications = injector.get(NotificationsService)
         this.translate = injector.get(TranslateService)
 
-        this.xterm = new Terminal({
+        const terminalOptions = {
             allowTransparency: true,
             allowProposedApi: true,
 overviewRuler: {
@@ -170,10 +170,11 @@ overviewRuler: {
             },
             reflowCursorLine: true,
             windowsPty: process.platform === 'win32' ? {
-                backend: this.configService.store.terminal.useConPTY ? 'conpty' : 'winpty',
+                backend: this.configService.store.terminal.useConPTY ? 'conpty' as const : 'winpty' as const,
                 buildNumber: getWindows10Build(),
             } : undefined,
-        })
+        }
+        this.xterm = new Terminal(terminalOptions)
         this.flowControl = new FlowControl(this.xterm)
         this.xtermCore = (this.xterm as any)._core
 
@@ -522,6 +523,17 @@ overviewRuler: {
             this.attachedHost.removeEventListener('contextmenu', this.boundContextMenu)
             this.attachedHost = undefined
         }
+    }
+
+    reactivateAfterVisibilityChange (): void {
+        this.resizeHandler()
+    }
+
+    deactivateAfterVisibilityChange (): void {
+        this.xterm.element?.querySelectorAll('canvas').forEach(c => {
+            c.height = c.width = 0
+            c.style.height = c.style.width = '0px'
+        })
     }
 
     destroy (): void {
